c796fe3bc05ab9273ef45a999851fec4a4a4333b
wellcomeai <19wellai96@gmail.com>
2025-09-15 10:06:03 +0300

    Create __init__.py

---
+++ admin/mixins/__init__.py
@@ -0,0 +1,9 @@
+"""
+Миксины для админ-панели
+"""
+
+from .menu_mixin import MenuMixin
+from .input_mixin import InputMixin
+from .navigation_mixin import NavigationMixin
+
+__all__ = ['MenuMixin', 'InputMixin', 'NavigationMixin']
